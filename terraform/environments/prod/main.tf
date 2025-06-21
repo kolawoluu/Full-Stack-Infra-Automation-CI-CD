8c922ddaa42f5ae0c25a0e6b90401495049744b4
Kolawolu <kolawolu@previ.se>
2025-06-21 14:40:54 +0100

    fix(terraform): run final format check

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -75,20 +75,20 @@ module "eks" {
 module "rds" {
   source = "../../modules/rds"
 
-  project_name               = var.project_name
-  environment                = var.environment
-  subnet_ids                 = module.vpc.private_subnet_ids
-  vpc_id                     = module.vpc.vpc_id
-  allowed_security_group_ids = [module.eks.cluster_security_group_id]
-  engine_version             = var.rds_engine_version
-  instance_class             = var.rds_instance_class
-  allocated_storage          = var.rds_allocated_storage
-  max_allocated_storage      = var.rds_max_allocated_storage
-  db_name                    = var.rds_db_name
-  db_username                = var.rds_db_username
-  db_password                = var.rds_db_password
-  backup_retention_period    = var.rds_backup_retention_period
-  deletion_protection        = var.rds_deletion_protection
+  project_name                    = var.project_name
+  environment                     = var.environment
+  subnet_ids                      = module.vpc.private_subnet_ids
+  vpc_id                          = module.vpc.vpc_id
+  allowed_security_group_ids      = [module.eks.cluster_security_group_id]
+  engine_version                  = var.rds_engine_version
+  instance_class                  = var.rds_instance_class
+  allocated_storage               = var.rds_allocated_storage
+  max_allocated_storage           = var.rds_max_allocated_storage
+  db_name                         = var.rds_db_name
+  db_username                     = var.rds_db_username
+  db_password                     = var.rds_db_password
+  backup_retention_period         = var.rds_backup_retention_period
+  deletion_protection             = var.rds_deletion_protection
   performance_insights_kms_key_id = aws_kms_key.main.arn
 
   depends_on = [module.vpc, module.eks]
